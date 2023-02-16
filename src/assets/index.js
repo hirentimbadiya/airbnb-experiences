1841dd5db9d899631a8c3b60012c3e1bd197f4da
heyhiru <hirentimbadiya74@gmail.com>
2023-02-16 20:49:20 +0530

    imported and exported images

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,4 +3,5 @@ import grid from "./photo-grid.png"
 import camel from "./camel.webp"
 import masi from "./masi.webp"
 import girl from "./girl.webp"
-export { airbnb, grid , camel , masi , girl};
+import hero from "./photo-grid.png"
+export { airbnb, grid , camel , masi , girl , hero};
