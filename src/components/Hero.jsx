f74fa18d7f45ff0b4a8d7e2a83ae433385ccecdf
heyhiru <hirentimbadiya74@gmail.com>
2023-02-20 17:37:26 +0530

    made divs similar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,13 +28,13 @@ const Hero = () => {
               </button>
             </div>
           </div>
-          <div className="flex h-[350px] w-[600px] msm:h-[400px] msm:w-[400px] relative rounded-xl ">
+          <div className=" flex h-[350px] w-[600px] msm:h-[400px] msm:w-[400px] mss:w-[300px] relative rounded-xl ">
             <img
               src={masi}
               alt=""
-              className="w-[100%] h-[100%] rounded-xl block absolute object-cover z-0"
+              className="w-[100%] h-[100%] rounded-xl block absolute object-cover msm:object-cover z-0"
             />
-            <div className=" z-[1] text-black p-4">
+            <div className="z-[1] text-black p-4">
               <h3 className="text-white font-poppins">Collection</h3>
               <div className="flex max-w-[70%]">
                 <h1 className="text-white font-poppins text-[20px] font-semibold">
@@ -42,24 +42,25 @@ const Hero = () => {
                 </h1>
               </div>
               <button
-                className=" mt-48 text-primary font-poppins bg-white
-               rounded-md w-[80px] p-1 font-medium  hover:bg-slate-100 shadow-xl"
+                className=" mt-48 text-primary font-poppins
+               bg-white rounded-md w-[80px] p-1 font-medium hover:bg-slate-100
+                shadow-xl"
               >
                 Show all
               </button>
             </div>
           </div>
-          <div className=" flex  h-[350px] w-[600px] msm:h-[400px] msm:w-[400px] relative rounded-xl ">
+          <div className=" flex h-[350px] w-[600px] msm:h-[400px] msm:w-[400px] mss:w-[300px] relative rounded-xl ">
             <img
               src={girl}
               alt=""
-              className="w-[100%] h-[100%] rounded-xl block absolute object-cover z-0"
+              className="w-[100%] h-[100%] rounded-xl block absolute object-cover msm:object-cover z-0"
             />
             <div className="z-[1] text-black p-4">
               <h3 className="text-white font-poppins">Collection</h3>
               <div className="flex max-w-[70%]">
                 <h1 className="text-white font-poppins text-[20px] font-semibold">
-                  Fun For the Friends and Friends
+                  Fun for the Family
                 </h1>
               </div>
               <button
