8df9071c759f8ee6d6480b45a15aa7cc61fe8a5a
heyhiru <hirentimbadiya74@gmail.com>
2023-02-25 17:45:11 +0530

    added images

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,4 +4,39 @@ import camel from "./camel.webp"
 import masi from "./masi.webp"
 import girl from "./girl.webp"
 import hero from "./photo-grid.png"
-export { airbnb, grid , camel , masi , girl , hero};
+import man from "./man.webp"
+import capman from "./capman.webp"
+import capgirl from "./capgirl.webp"
+import miamigirl from "./miamigirl.webp"
+import pizzaaunty from "./pizzaaunty.webp"
+import couple from "./couple.webp"
+import laptoplady from "./laptoplady.webp"
+import giraf from "./giraf.webp"
+import birthdaylady from "./birthdaylady.webp"
+import wowlady from "./wowlady.webp"
+import puppyman from "./puppyman.webp"
+import cardman from "./cardman.webp"
+import redgreen from "./redgreen.webp"
+import japanlady from "./japanlady.webp"
+import mexicoman from "./mexicoman.webp"
+import teacher from "./teacher.webp"
+import ninjachef from "./ninjachef.webp"
+import whitechef from "./whitechef.webp"
+import naninoodle from "./naninoodle.webp"
+import eggchef from "./eggchef.webp"
+import bozygirl from "./bozygirl.webp"
+import bear from "./bear.webp"
+import monk from "./monk.webp"
+import polandchef from "./polandchef.webp"
+import ranikivav from "./ranikivav.webp"
+import reader from "./reader.webp"
+import photographer from "./photographer.webp"
+import ghummat from "./ghummat.webp"
+import lemon from "./lemon.webp"
+
+export {
+    airbnb, grid, camel, giraf, birthdaylady, puppyman, cardman, wowlady,
+    redgreen, japanlady, masi, girl, hero, man, capman, capgirl,
+    miamigirl, pizzaaunty, couple, laptoplady, mexicoman, bear, bozygirl, eggchef, naninoodle, whitechef,
+    ninjachef, teacher, monk, polandchef, ranikivav, reader, photographer, ghummat, lemon
+};
