2837943aba780c4e994b7f21a37e8fd5052355c6
heyhiru <hirentimbadiya74@gmail.com>
2023-02-25 17:45:35 +0530

    added data

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,6 @@
-const Buttons = {
+import { bear, birthdaylady, bozygirl, capgirl, capman, cardman, couple, eggchef, ghummat, giraf, japanlady, laptoplady, lemon, man, mexicoman, miamigirl, monk, naninoodle, ninjachef, photographer, pizzaaunty, polandchef, puppyman, ranikivav, reader, redgreen, teacher, whitechef, wowlady } from "../assets";
+
+export const Buttons = {
     fThree: ["Dates", "Group size", "More filters"],
     others: ["Great for Groups", "Family-friendly", "Animals", "Arts & writing", "Baking",
         "Cooking", "Dance", "Drinks", "Entertainment", "Fitness",
@@ -6,5 +8,266 @@ const Buttons = {
         "Olympians & paralympians", "Designed for accessibility"]
 }
 
+export const CardsSlider = [
+    {
+        id: 1,
+        img: man,
+        country: "United States",
+        rating: 4.99,
+        ratingNum: 158,
+        price: 1574,
+        title: "Plan the Perfect New York Vacation"
+    },
+    {
+        id: 2,
+        img: capman,
+        country: "South Korea",
+        rating: 4.98,
+        ratingNum: 67,
+        price: 3668,
+        title: "Craft a Custom to Korea with Jae"
+    },
+    {
+        id: 4,
+        img: pizzaaunty,
+        country: "Italy",
+        rating: 5.0,
+        ratingNum: 112,
+        price: 2700,
+        title: "Plan a Venice venture with Giulia"
+    },
+    {
+        id: 5,
+        img: couple,
+        country: "Spain",
+        rating: 4.98,
+        ratingNum: 45,
+        price: 2034,
+        title: "Plan a Perfect Barcelona Vacation"
+    },
+    {
+        id: 6,
+        img: miamigirl,
+        country: "United States",
+        rating: 4.93,
+        ratingNum: 77,
+        price: 2123,
+        title: "Plan a Perfect Trip to Sunny Miami",
+    },
+    {
+        id: 7,
+        img: laptoplady,
+        country: "United States",
+        rating: 5.0,
+        ratingNum: 7,
+        price: 2071,
+        title: "Discover Sunny Fort Lauderable",
+    },
+    {
+        id: 3,
+        img: capgirl,
+        country: "United States",
+        rating: 5.0,
+        ratingNum: 89,
+        price: 1248,
+        title: "Plan a Trip To Nashville"
+    },
+]
 
-export { Buttons };
+export const TopSellerSlider = [
+    {
+        id: 1,
+        img: giraf,
+        country: "South Africa",
+        rating: 4.92,
+        ratingNum: 658,
+        price: 1698,
+        title: "Live Virtual Safari In South Africa"
+    },
+    {
+        id: 2,
+        img: puppyman,
+        country: "Ireland",
+        rating: 4.97,
+        ratingNum: 2264,
+        price: 1501,
+        title: "Explore Irish History , Culture & Craic..."
+    },
+    {
+        id: 3,
+        img: wowlady,
+        country: "France",
+        rating: 5.0,
+        ratingNum: 6458,
+        price: 2945,
+        title: "From Paris With Laughter"
+    },
+    {
+        id: 4,
+        img: redgreen,
+        country: "Ireland",
+        rating: 4.9,
+        ratingNum: 68,
+        price: 1243,
+        title: "Saints & Sinners , St Patrick's OG Game"
+    },
+    {
+        id: 5,
+        img: cardman,
+        country: "England",
+        rating: 4.99,
+        ratingNum: 475,
+        price: 2023,
+        title: "Explore Magic and Mentalism"
+    },
+    {
+        id: 6,
+        img: birthdaylady,
+        country: "Spain",
+        rating: 4.91,
+        ratingNum: 2648,
+        price: 1301,
+        title: "'No Spain No Game' The Fabulous Game"
+    },
+    {
+        id: 7,
+        img: japanlady,
+        country: "Japan",
+        rating: 4.98,
+        ratingNum: 531,
+        price: 750,
+        title: "Origami and Essence of Japan"
+    }
+]
+
+export const GreatForGrp = [
+    {
+        id: 1,
+        img: bear,
+        country: "South Africa",
+        rating: 4.97,
+        ratingNum: 38,
+        price: 850,
+        title: "A Breif History of Wine In South Africa"
+    },
+    {
+        id: 2,
+        img: teacher,
+        country: "Portugal",
+        rating: 4.97,
+        ratingNum: 919,
+        price: 4816,
+        title: "Astrology Workshop , Birth Chart Reading"
+    },
+    {
+        id: 3,
+        img: naninoodle,
+        country: "Australia",
+        rating: 5.0,
+        ratingNum: 8300,
+        price: 2878,
+        title: "Pastas With the Grandmas"
+    },
+    {
+        id: 4,
+        img: whitechef,
+        country: "Italy",
+        rating: 4.99,
+        ratingNum: 768,
+        price: 3504,
+        title: "Champion Tiramisu With Italian Chef"
+    },
+    {
+        id: 5,
+        img: ninjachef,
+        country: "Japan",
+        rating: 4.96,
+        ratingNum: 1383,
+        price: 2193,
+        title: "Joy to the Team - A Vertual Magic Show"
+    },
+    {
+        id: 6,
+        img: bozygirl,
+        country: "Brazil",
+        rating: 4.97,
+        ratingNum: 868,
+        price: 2400,
+        title: "Boozy Drag Queen Bingo"
+    },
+    {
+        id: 7,
+        img: eggchef,
+        country: "Japan",
+        rating: 4.98,
+        ratingNum: 566,
+        price: 1750,
+        title: "Ramen in Japan With Master Chef"
+    }
+]
+
+export const WeekendPlanner = [
+    {
+        id: 1,
+        img: monk,
+        country: "Japan",
+        rating: 4.94,
+        ratingNum: 1939,
+        price: 1222,
+        title: "Chanting Meditation with a Japanese Monk"
+    },
+    {
+        id: 2,
+        img: photographer,
+        country: "Malta",
+        rating: 4.97,
+        ratingNum: 52,
+        price: 1214,
+        title: "Valleta Virtual Tour"
+    },
+    {
+        id: 3,
+        img: polandchef,
+        country: "Poland",
+        rating: 5.0,
+        ratingNum: 428,
+        price: 1855,
+        title: "Polish Pierogi Cooking Class"
+    },
+    {
+        id: 4,
+        img: reader,
+        country: "India",
+        rating: 4.99,
+        ratingNum: 559,
+        price: 6449,
+        title: "Vedic Astrology Reading"
+    },
+    {
+        id: 5,
+        img: ranikivav,
+        country: "India",
+        rating: 4.97,
+        ratingNum: 2102,
+        price: 1099,
+        title: "Plan & Design a Trip - Increadible India Unexplored"
+    },
+    {
+        id: 6,
+        img: lemon,
+        country: "Mexico",
+        rating: 4.99,
+        ratingNum: 458,
+        price: 2499,
+        title: "Advanced Taco Tuesday With a Pro Chef"
+    },
+    {
+        id: 7,
+        img: ghummat,
+        country: "India",
+        rating: 5.0,
+        ratingNum: 399,
+        price: 1399,
+        title: "The Might and Beauty Of the Great Mughal's- Agra Fort"
+    }
+]
